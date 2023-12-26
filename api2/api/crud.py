b4a41378aeaaee371036a21b577632ecf4261dfc
hyusap <paulayush@gmail.com>
2023-12-26 13:01:02 -0500

    add metacognitions

--- api2/api/crud.py
+++ api2/api/crud.py
@@ -40,10 +40,32 @@ def create_session(db: Session, user_id: str, session: schemas.SessionCreate):
 def create_message(db: Session, message: schemas.MessageCreate, session_id: int):
     db_message = models.Message(
         session_id=session_id,
-        message_type=message.message_type,
+        is_user=message.is_user,
         content=message.content,
     )
     db.add(db_message)
     db.commit()
     db.refresh(db_message)
     return db_message
+
+
+def get_metacognitions(db: Session, message_id: int):
+    return (
+        db.query(models.Metacognitions)
+        .filter(models.Metacognitions.message_id == message_id)
+        .all()
+    )
+
+
+def create_metacognition(
+    db: Session, metacognition: schemas.MetacognitionsCreate, message_id: int
+):
+    db_metacognition = models.Metacognitions(
+        message_id=message_id,
+        metacognition_type=metacognition.metacognition_type,
+        content=metacognition.content,
+    )
+    db.add(db_metacognition)
+    db.commit()
+    db.refresh(db_metacognition)
+    return db_metacognition
