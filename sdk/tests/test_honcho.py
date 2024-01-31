5341ebf7e27f946000f00244aa06bc6ec7a60346
hyusap <paulayush@gmail.com>
2024-01-31 11:12:48 -0500

    🧪 asyncify tests

--- sdk/tests/test_honcho.py
+++ sdk/tests/test_honcho.py
@@ -1,58 +1,64 @@
+import pytest
 from honcho import Client
 from uuid import uuid1
 
 
-def test_session_creation_retrieval():
+@pytest.mark.asyncio
+async def test_session_creation_retrieval():
     client = Client("http://localhost:8000")
     user_id = str(uuid1())
-    created_session = client.create_session(user_id)
-    retrieved_session = client.get_session(user_id, created_session.id)
+    created_session = await client.create_session(user_id)
+    retrieved_session = await client.get_session(user_id, created_session.id)
     assert retrieved_session.id == created_session.id
     assert retrieved_session.is_active == True
     assert retrieved_session.location_id == "default"
     assert retrieved_session.session_data == {}
 
 
-def test_session_multiple_retrieval():
+@pytest.mark.asyncio
+async def test_session_multiple_retrieval():
     client = Client("http://localhost:8000")
     user_id = str(uuid1())
-    created_session_1 = client.create_session(user_id)
-    created_session_2 = client.create_session(user_id)
-    retrieved_sessions = client.get_sessions(user_id)
+    created_session_1 = await client.create_session(user_id)
+    created_session_2 = await client.create_session(user_id)
+    retrieved_sessions = await client.get_sessions(user_id)
     assert len(retrieved_sessions) == 2
     assert retrieved_sessions[0].id == created_session_1.id
     assert retrieved_sessions[1].id == created_session_2.id
 
 
-def test_session_update():
+@pytest.mark.asyncio
+async def test_session_update():
     user_id = str(uuid1())
     client = Client("http://localhost:8000")
-    created_session = client.create_session(user_id)
-    assert created_session.update({"foo": "bar"})
-    retrieved_session = client.get_session(user_id, created_session.id)
+    created_session = await client.create_session(user_id)
+    assert await created_session.update({"foo": "bar"})
+    retrieved_session = await client.get_session(user_id, created_session.id)
     assert retrieved_session.session_data == {"foo": "bar"}
 
 
-def test_session_deletion():
+@pytest.mark.asyncio
+async def test_session_deletion():
     user_id = str(uuid1())
     client = Client("http://localhost:8000")
-    created_session = client.create_session(user_id)
+    created_session = await client.create_session(user_id)
     assert created_session.is_active == True
-    created_session.delete()
+    await created_session.delete()
     assert created_session.is_active == False
-    retrieved_session = client.get_session(user_id, created_session.id)
+    retrieved_session = await client.get_session(user_id, created_session.id)
     assert retrieved_session.is_active == False
     assert retrieved_session.id == created_session.id
 
 
-def test_messages():
+@pytest.mark.asyncio
+async def test_messages():
     user_id = str(uuid1())
     client = Client("http://localhost:8000")
-    created_session = client.create_session(user_id)
-    created_session.create_message(is_user=True, content="Hello")
-    created_session.create_message(is_user=False, content="Hi")
-    retrieved_session = client.get_session(user_id, created_session.id)
-    messages = retrieved_session.get_messages()
+    created_session = await client.create_session(user_id)
+    await created_session.create_message(is_user=True, content="Hello")
+    await created_session.create_message(is_user=False, content="Hi")
+    retrieved_session = await client.get_session(user_id, created_session.id)
+    messages = await retrieved_session.get_messages()
     assert len(messages) == 2
     user_message, ai_message = messages
     assert user_message.content == "Hello"
