4cfe15b17a5212be7f096cbc714490b9a6afa928
vintro <vince@plasticlabs.ai>
2024-02-22 20:03:45 -0500

    working, but weird compiler error

--- example/discord/honcho-dspy-personas/bot.py
+++ example/discord/honcho-dspy-personas/bot.py
@@ -3,6 +3,7 @@ from uuid import uuid1
 import discord
 from honcho import Honcho
 from graph import chat
+from dspy import Example
 from chain import langchain_message_converter
 
 intents = discord.Intents.default()
@@ -14,8 +15,8 @@ intents.reactions = True  # Enable reactions intent
 # app_id = str(uuid1())
 app_name = "vince-dspy-personas"
 
-# honcho = Honcho(app_name=app_name, base_url="http://localhost:8000") # uncomment to use local
-honcho = Honcho(app_name=app_name)  # uses demo server at https://demo.honcho.dev
+honcho = Honcho(app_name=app_name, base_url="http://localhost:8000") # uncomment to use local
+# honcho = Honcho(app_name=app_name)  # uses demo server at https://demo.honcho.dev
 honcho.initialize()
 
 bot = discord.Bot(intents=intents)
@@ -51,7 +52,7 @@ async def on_message(message):
     user = honcho.get_or_create_user(user_id)
     location_id = str(message.channel.id)
 
-    sessions = list(user.get_sessions_generator(location_id))
+    sessions = list(user.get_sessions_generator(location_id, is_active=True, reverse=True))
 
     if len(sessions) > 0:
         session = sessions[0]
@@ -82,32 +83,48 @@ async def on_reaction_add(reaction, user):
     if user == bot.user:
         return
 
-    user_id = f"discord_{str(reaction.message.author.id)}"
-    user = honcho.get_or_create_user(user_id)
+    user_id = f"discord_{str(user.id)}"
+    honcho_user = honcho.get_or_create_user(user_id)
     location_id = str(reaction.message.channel.id)
+
+    sessions = list(honcho_user.get_sessions_generator(location_id, is_active=True, reverse=True))
+    if len(sessions) > 0:
+        session = sessions[0]
+    else:
+        session = honcho_user.create_session(location_id)
+
+    messages = list(session.get_messages_generator(reverse=True))
+    ai_responses = [message for message in messages if not message.is_user]
+    user_responses = [message for message in messages if message.is_user]
+    # most recent AI response
+    ai_response = ai_responses[0].content
+    user_response = user_responses[0]
+
+    user_state_storage = dict(honcho_user.metadata)
+    user_state = list(session.get_metamessages_generator(metamessage_type="user_state", message=user_response, reverse=True))[0].content
+    examples = user_state_storage[user_state]["examples"]
 
     # Check if the reaction is a thumbs up
     if str(reaction.emoji) == "👍":
-        thumbs_up_messages.append(reaction.message.content)
-        print(f"Added to thumbs up: {reaction.message.content}")
+        example = Example(
+            chat_input=user_response.content,  
+            response=ai_response,
+            assessment_dimension=user_state,
+            label='yes'
+        ).with_inputs("chat_input", "response", "assessment_dimension")
+        examples.append(example.toDict())
     # Check if the reaction is a thumbs down
     elif str(reaction.emoji) == "👎":
-        thumbs_down_messages.append(reaction.message.content)
-        print(f"Added to thumbs down: {reaction.message.content}")
+        example = Example(
+            chat_input=user_response.content,  
+            response=ai_response,
+            assessment_dimension=user_state,
+            label='no'
+        ).with_inputs("chat_input", "response", "assessment_dimension")
+        examples.append(example.toDict())
 
-    # TODO: we need to append these to the examples list within the user state json object
-    # append example
-    # example = Example(chat_input=chat_input, assessment_dimension=user_state, response=response).with_inputs('chat_input')
-    # examples.append(example)
-    # user_state_storage[user_state]["examples"] = examples
-    example = Example(
-        chat_input=chat_input, assessment_dimension=user_state, response=response
-    ).with_inputs("chat_input")
-    user_state_storage = dict(user.metadata)
-    examples = user_state_storage.get("examples", [])
-    examples.append(example)
-    user_state_storage["examples"] = examples
-    user.update(metadata=user_state_storage)
+    user_state_storage[user_state]["examples"] = examples
+    honcho_user.update(metadata=user_state_storage)
 
 
 @bot.slash_command(name="restart", description="Restart the Conversation")
@@ -115,7 +132,7 @@ async def restart(ctx):
     user_id = f"discord_{str(ctx.author.id)}"
     user = honcho.get_or_create_user(user_id)
     location_id = str(ctx.channel_id)
-    sessions = list(user.get_sessions_generator(location_id))
+    sessions = list(user.get_sessions_generator(location_id, reverse=True))
     sessions[0].close() if len(sessions) > 0 else None
 
     msg = (
