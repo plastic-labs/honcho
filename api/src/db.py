8be7a9e937d95a8f3c11cb5cceb1f079604d281d
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2024-02-29 00:36:09 -0800

    Refactored code but need to tweak asyncpg

--- api/src/db.py
+++ api/src/db.py
@@ -1,20 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+# from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 
 connect_args = {}
 
-if os.environ["DATABASE_TYPE"] == "sqlite": # https://fastapi.tiangolo.com/tutorial/sql-databases/#note
+if (
+    os.environ["DATABASE_TYPE"] == "sqlite"
+):  # https://fastapi.tiangolo.com/tutorial/sql-databases/#note
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(
+engine = create_async_engine(
     os.environ["CONNECTION_URI"], connect_args=connect_args, echo=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
