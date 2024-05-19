881009b4d464a6ce8aac0a428cccfb828aa76110
vintro <vince@plasticlabs.ai>
2024-05-19 19:22:12 -0400

    initial prompts, logic

--- src/deriver.py
+++ src/deriver.py
@@ -29,64 +29,130 @@ if SENTRY_ENABLED:
 # SUPABASE_ID = os.getenv("SUPABASE_ID")
 # SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
-
-class DeriveFacts(OpenAICall):
+# user prediction thought + additional data to improve prediction
+class UserPredictionThought(OpenAICall):
     prompt_template = """
-    You are tasked with deriving discrete facts about the user based on their input. The goal is to only extract absolute facts from the message, do not make inferences beyond the text provided.
+    USER:
+    Generate a "thought" that makes a theory of mind prediction about what the user will say based on the way the conversation has been going. Also list other pieces of data that would help improve your prediction.
 
-    chat history: ```{chat_history}```
-    user input: ```{user_input}```
+    Conversation:
+    ```
+    {chat_history}
+    ```
 
-    Output the facts as a numbered list.
+    Generate the additional pieces of data as a numbered list.
+
+    ASSISTANT:
+    Thought:
     """
 
     chat_history: str
-    user_input: str
-
     call_params = OpenAICallParams(model="gpt-4o-2024-05-13")
 
-
-class CheckDups(OpenAICall):
+# user prediction thought revision given context
+class UserPredictionThoughtRevision(OpenAICall):
     prompt_template = """
-    Your job is to determine if the new fact exists in the old:
+    USER:
+    You are tasked with revising theory of mind "thoughts" about what the user is going to say. Here is the thought generated previously:
 
-    Old: ```{existing_facts}```
+    Thought: ```
+    {user_prediction_thought}
+    ```
 
-    New: ```{fact}```
+    Based on this thought, the following personal data has been retrieved:
+
+    Personal Data: ```
+    {retrieved_vectors}
+    ```
+
+    And here's the conversation history that was used to generate the original thought:
+
+    History: ```
+    {chat_history}
+    ```
     
-    If the new fact is sufficiently represented in the old list, return False. Otherwise, if the fact is indeed new, return True.
-    """
-    existing_facts: List[str]
-    fact: str
+    Given the thought, conversation history, and personal data, revise the thought. If there are no changes to be made, output "None".
 
+    ASSISTANT:
+    thought revision:
+    """
+    user_prediction_thought_revision: str
+    retrieved_vectors = str
+    chat_history: str
+    call_params = OpenAICallParams(model="gpt-4o-2024-05-13")
+
+# VoE thought
+class VoeThought(OpenAICall):
+    prompt_template = """
+    USER:
+    Below is a "thought" about what the user was going to say, and then what the user actually said. Generate a theory of mind prediction about the user based on the difference between the "thought" and actual response.
+
+    Thought: ```
+    {user_prediction_thought_revision}
+    ```
+
+    Actual: ```
+    {actual}
+    ```
+
+    Provide the theory of mind prediction solely in reference to the Actual statement, i.e. do not generate something that negates the thought. Do not speculate anything about the user.
+    """
+    user_prediction_thought_revision: str
+    actual: str
+    call_params = OpenAICallParams(model="gpt-4o-2024-05-13")
+
+# VoE derive facts
+class VoeDeriveFacts(OpenAICall):
+    prompt_template = """
+    USER:
+    Below is the most recent AI message we sent to a user, a "thought" about what the user was going to say to that, what the user actually responded with, and a theory of mind prediction about the user's response. Derive a fact (or list of facts) about the user based on the difference between the original thought and their actual response plus the theory of mind prediction about that response.
+
+    Most recent AI message: ```
+    {ai_message}
+    ```
+
+    Thought about what they were going to say: ```
+    {user_prediction_thought_revision}
+    ```
+
+    Actual response: ```
+    {actual}
+    ```
+
+    Theory of mind prediction about that response: ```
+    {voe_thought}
+    ```
+
+    Provide the fact(s) solely in reference to the Actual response and theory of mind prediction about that response; i.e. do not derive a fact that negates the thought about what they were going to say. Do not speculate anything about the user. Each fact must contain enough specificity to stand alone. If there are many facts, list them out. Your response should be a numbered list with each item on a new line, for example: `\n\n1. foo\n\n2. bar\n\n3. baz`. If there's nothing to derive (i.e. the statements are sufficiently similar), print "None".
+    """
+    user_prediction_thought_revision: str
+    actual: str
+    voe_thought: str
+    call_params = OpenAICallParams(model="gpt-4o-2024-05-13")
+
+# check dups
+class CheckVoeList(OpenAICall):
+    prompt_template = """
+    USER:
+    Please compare the following two lists and keep only unique items:
+
+    Old: ```
+    {existing_facts}
+    ```
+
+    New: ```
+    {facts}
+    ```
+    
+    Remove redundant information from the new list and output the remaining facts. Your response should be a numbered list with each fact on a new line, for example: `\n\n1. foo\n\n2. bar\n\n3. baz`. If there's nothing to remove (i.e. the statements are sufficiently different), print "None".
+    """
+    existing_facts: str
+    facts: str
     call_params = OpenAICallParams(model="gpt-4o-2024-05-13")
 
 
 async def process_item(db: AsyncSession, payload: dict):
-    # print(payload["record"]["is_user"])
-    # print(type(payload["record"]["is_user"]))
-    # if payload["record"]["is_user"]:  # Check if the message is from a user
-    #     session_id = payload["record"]["session_id"]
-    #     message_id = payload["record"]["id"]
-    #     content = payload["record"]["content"]
 
-    #     # Example of querying for a user_id based on session_id, adjust according to your schema
-    #     session: models.Session
-    #     user_id: uuid.UUID
-    #     app_id: uuid.UUID
-    #     async with SessionLocal() as db:
-    #         stmt = (
-    #             select(models.Session)
-    #             .join(models.Session.messages)
-    #             .where(models.Message.id == message_id)
-    #             .where(models.Session.id == session_id)
-    #             .options(selectinload(models.Session.user))
-    #         )
-    #         result = await db.execute(stmt)
-    #         session = result.scalars().one()
-    #         user = session.user
-    #         user_id = user.id
-    #         app_id = user.app_id
     collection: models.Collection
     # async with SessionLocal() as db:
     collection = await crud.get_collection_by_name(
@@ -123,7 +189,7 @@ async def process_user_message(
     db: AsyncSession,
 ):
     """
-    Process a user message and derive facts from it (check for duplicates before writing to the collection).
+    Process a user message thought the VoE pipeline
     """
     messages_stmt = await crud.get_messages(
         db=db, app_id=app_id, user_id=user_id, session_id=session_id, reverse=True
@@ -141,6 +207,29 @@ async def process_user_message(
     chat_history_str = "\n".join(
         [f"user: {m.content}" if m.is_user else f"ai: {m.content}" for m in messages]
     )
+
+    ############
+
+    # user prediction thought
+    user_prediction_thought = UserPredictionThought(chat_history=chat_history_str)
+    user_prediction_thought_response = await user_prediction_thought.call_async()
+
+    ## query the dialectic api to build the context
+    additional_data = re.findall(r"\d+\.\s([^\n]+)", user_prediction_thought_response.content)
+
+    for datum in additional_data:
+        
+
+    # user prediction thought revision given the context
+
+    # VoE thought
+
+    # VoE derive facts
+
+    # check dups
+
+    ###############
+
     facts_response = await DeriveFacts(
         chat_history=chat_history_str, user_input=content
     ).call_async()
