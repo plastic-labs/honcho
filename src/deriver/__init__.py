245c5212092116e7760dbd4ce28d157c0ec3b53d
vintro <vince@plasticlabs.ai>
2024-09-27 10:19:25 -0400

    cleanup from timing removal

--- src/deriver/__init__.py
+++ src/deriver/__init__.py
@@ -1,4 +1,3 @@
 from .consumer import process_item
-from .timing import timing_decorator, csv_file_path
 
-__all__ = ['process_item', 'timing_decorator', 'csv_file_path']
+__all__ = ['process_item']
