a4b98f3d0a497d4f251fe14cc3d8700672aaf92a
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2024-12-09 18:21:50 -0500

    fix: increase db pool limit and optimize crud requests

--- src/db.py
+++ src/db.py
@@ -21,10 +21,15 @@ engine = create_async_engine(
     connect_args=connect_args,
     echo=True,
     pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=50,
 )
 
 SessionLocal = async_sessionmaker(
-    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    bind=engine,
 )
 
 table_schema = os.getenv("DATABASE_SCHEMA")
