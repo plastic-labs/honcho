f289c50649a2bea9f7e7eeae139f8267dd849e11
Daniel Balcells <dbalcells@gmail.com>
2025-02-03 13:09:36 +0100

    Add system prompt and note on format

--- src/deriver/tom/single_prompt.py
+++ src/deriver/tom/single_prompt.py
@@ -57,6 +57,8 @@ EXPECTATION VIOLATIONS:
 - Format: "POTENTIAL SURPRISE: [possible content] [reason] [confidence level]"
 - Include 3-5 possible surprises
 </prediction>
+
+Important: always wrap your prediction in <prediction> tags.
 """
 
         messages = [
