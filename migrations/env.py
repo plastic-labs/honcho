32577854dd136841b8adb0ad5707bfa7d046a8fd
Benjamin McCormick <docterformer@protonmail.com>
2025-05-13 12:38:28 -0400

    fix: handle all possible env vals for DATABASE_SCHEMA

--- migrations/env.py
+++ migrations/env.py
@@ -37,6 +37,9 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
+if target_metadata.schema is None or target_metadata.schema == "":
+    target_metadata.schema = "public"
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
