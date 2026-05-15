746efe4ab6b47cbff645fa00e8d7170de22eba27
Felipe de Leon <felipe@felipedeleon.com>
2026-05-15 10:49:53 +0400

    feat(api): add deduplication support to conclusions endpoint

--- src/routers/conclusions.py
+++ src/routers/conclusions.py
@@ -41,6 +41,7 @@ async def create_conclusions(
         db,
         observations=body.conclusions,
         workspace_name=workspace_id,
+        deduplicate=body.deduplicate,
     )
 
     logger.debug(
